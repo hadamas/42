9e34e67b6bf01186c42b67255c63c29c596452fb
Alanis Hadama <ahadama-@student.42.rio>
2023-11-07 13:48:44 -0300

    primeiro e unico commit

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,25 +6,25 @@
 /*   By: ahadama- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:57:12 by ahadama-          #+#    #+#             */
-/*   Updated: 2023/11/05 12:43:31 by ahadama-         ###   ########.fr       */
+/*   Updated: 2023/11/07 11:50:16 by ahadama-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *str1, const void *str2, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t		i;
 
-	if (!str1 && !str2)
+	if (!dest && !src)
 		return (NULL);
 	i = 0;
-	if ((size_t)str1 - (size_t)str2 < n)
+	if ((size_t)dest - (size_t)src < n)
 	{
 		i = n - 1;
 		while (i >= 0 && i < n)
 		{
-			((unsigned char *)str1)[i] = ((unsigned char *)str2)[i];
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 			i--;
 		}
 	}
@@ -32,9 +32,9 @@ void	*ft_memmove(void *str1, const void *str2, size_t n)
 	{
 		while (i < n)
 		{
-			((unsigned char *)str1)[i] = ((unsigned char *)str2)[i];
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 			i++;
 		}
 	}
-	return (str1);
+	return (dest);
 }
